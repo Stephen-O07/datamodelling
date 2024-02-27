2fcc409737252cba7dbca659c6f4ae710b6c899a
Stephen-O07 <osamezu.stephen@yahoo.com>
2024-02-27 22:20:53 +0000

    create customers sales table

---
+++ sql_statements.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Sales.Customers
+(
+  custid       INT          NOT NULL,
+  companyname  NVARCHAR(40) NOT NULL,
+  contactname  NVARCHAR(30) NOT NULL,
+  contacttitle NVARCHAR(30) NOT NULL,
+  address      NVARCHAR(60) NOT NULL,
+  city         NVARCHAR(15) NOT NULL,
+  region       NVARCHAR(15) NULL,
+  postalcode   NVARCHAR(10) NULL,
+  country      NVARCHAR(15) NOT NULL,
+  phone        NVARCHAR(24) NOT NULL,
+  fax          NVARCHAR(24) NULL,
+);
